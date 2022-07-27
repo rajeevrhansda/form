56151ed24c4cf36802b454a97fe08e45166df2b1
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-27 16:13:12 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import FormInput from './components/FormInput';
 
 function App() {
+  const [username, setUsername] = useState("");
+  console.log(username);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form>
+        <FormInput placeholder="Username" setUsername={setUsername} />
+        <FormInput placeholder="Email" />
+        <FormInput placeholder="Full Name" />
+        <FormInput placeholder="Something" />
+      </form>
     </div>
   );
 }
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,10 @@
+import './formInput.css'
+
+export default function FormInput(props) {
+    return (
+        <div className='formInput'>
+            {/* <label>Username</label> */}
+            <input placeholder={props.placeholder} onChange={e=>props.setUsername(e.target.value)} />
+        </div>
+    )
+}
